cce62a349b056d0f55834abf6cc0f2794e3fd4e9
Yuting Wang <yuting_wang@progressive.com>
2022-12-05 15:23:17 -0500

    Break tracker.

---
+++ HelloWorldSolution/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HelloWorld;
+
+Console.WriteLine("Hello, Jessica!");
+Console.WriteLine("Take a break");
+Console.Write("How many minutes:");
+string? minutes = Console.ReadLine();
+
+if (minutes is not null)
+{
+    DateUtils helper = new DateUtils();
+
+    int mins = int.Parse(minutes);
+    DateTime endOfBreak = helper.TakeABreak(mins);
+
+    Console.WriteLine($"Alright. Come back at {endOfBreak}.");
+}
+else
+{
+    Console.WriteLine("Don't come back maybe.");
+}
